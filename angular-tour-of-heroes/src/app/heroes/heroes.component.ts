d0d7c040ace8c17c1a03972e2feab7f1debcd1ea
Kim HyunWoo <stories2@naver.com>
2018-07-30 17:21:16 +0900

    End of chapter 6

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -13,17 +14,24 @@ export class HeroesComponent implements OnInit {
     name: "Windstorm"
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     //this.selectedHero = this.hero;
+    this.getHeroes();
   }
 
   onSelect(eventHero: Hero): void {
     this.selectedHero = eventHero;
+  }
+
+  getHeroes(): void {
+    /*this.heroes = this.heroService.getHeroes();*/
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
